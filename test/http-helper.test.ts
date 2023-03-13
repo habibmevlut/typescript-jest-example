14d6963371af6f7f2b39a0bbdf9eb23fa9f04f96
habib <habib.mevlut@pia-team.com>
2023-03-13 16:19:58 +0300

    test with helpers

--- test/http-helper.test.ts
+++ test/http-helper.test.ts
@@ -1,4 +1,4 @@
-import { createUser, deleteUser, getUser, getUsers } from "../src/http-call";
+import { createUser, deleteUser, getUser, getUsers, updateUser, User } from "../src/http-call";
 import fetchMock from 'jest-fetch-mock';
 
 // import fetch from 'node-fetch';
@@ -41,59 +41,59 @@ describe('HttpRequest Success test cases', () => {
         });
     });
 
-    // describe('PUT updateUser', () => {
-    //     beforeEach(() => {
-    //         jest.resetAllMocks();
-    //     });
-    //
-    //     test('should call the put function with the correct URL and data', async () => {
-    //         const user: User = {
-    //             id: 1,
-    //             name: 'John Doe',
-    //             email: 'john@example.com',
-    //         };
-    //         const expectedResponse: User = {
-    //             id: 1,
-    //             name: 'Jane Doe',
-    //             email: 'jane@example.com',
-    //         };
-    //
-    //         // mock the fetch response
-    //         fetchMock.mockOnce(JSON.stringify(expectedResponse), {
-    //             status: 200,
-    //             headers: {'Content-Type': 'application/json'},
-    //         });
-    //
-    //         const response = await updateUser(user);
-    //         console.log(fetchMock.mock)
-    //         expect(fetchMock.mock.calls.length).toBe(1);
-    //         expect(fetchMock.mock.calls[0][0]).toBe(`https://jsonplaceholder.typicode.com/users/${user.id}`);
-    //         expect(fetchMock.mock.calls[0][1]).toMatchObject({
-    //             method: 'PUT',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify(user),
-    //         });
-    //         expect(response).toEqual(expectedResponse);
-    //     });
-    //
-    //     test('should throw an error if the fetch response is not ok', async () => {
-    //         const user: User = {
-    //             id: 1,
-    //             name: 'John Doe',
-    //             email: 'john@example.com',
-    //         };
-    //
-    //         // mock the fetch response to have a non-ok status
-    //         fetchMock.mockOnce(JSON.stringify({}), {
-    //             status: 400,
-    //             headers: {'Content-Type': 'application/json'},
-    //         });
-    //
-    //         await expect(updateUser(user)).rejects.toThrowError('Failed to put');
-    //     });
-    // });
+    describe('PUT updateUser', () => {
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+
+        test('should call the put function with the correct URL and data', async () => {
+            const user: User = {
+                id: 1,
+                name: 'John Doe',
+                email: 'john@example.com',
+            };
+            const expectedResponse: User = {
+                id: 1,
+                name: 'Jane Doe',
+                email: 'jane@example.com',
+            };
+
+            // mock the fetch response
+            fetchMock.mockOnce(JSON.stringify(expectedResponse), {
+                status: 200,
+                headers: {'Content-Type': 'application/json'},
+            });
+
+            const response = await updateUser(user);
+            console.log(fetchMock.mock)
+            expect(fetchMock.mock.calls.length).toBe(1);
+            expect(fetchMock.mock.calls[0][0]).toBe(`https://jsonplaceholder.typicode.com/users/${user.id}`);
+            expect(fetchMock.mock.calls[0][1]).toMatchObject({
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user),
+            });
+            expect(response).toEqual(expectedResponse);
+        });
+
+        test('should throw an error if the fetch response is not ok', async () => {
+            const user: User = {
+                id: 1,
+                name: 'John Doe',
+                email: 'john@example.com',
+            };
+
+            // mock the fetch response to have a non-ok status
+            fetchMock.mockOnce(JSON.stringify({}), {
+                status: 400,
+                headers: {'Content-Type': 'application/json'},
+            });
+
+            await expect(updateUser(user)).rejects.toThrowError('Failed to put');
+        });
+    });
 
     describe('DELETE User', () => {
         beforeEach(() => {
@@ -101,7 +101,7 @@ describe('HttpRequest Success test cases', () => {
         });
 
         test('should make a DELETE request to the correct URL', async () => {
-            const mockResponse = { ok: true };
+            const mockResponse = {ok: true};
             fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
             const userId = 1123;
             await deleteUser(userId);
@@ -110,18 +110,18 @@ describe('HttpRequest Success test cases', () => {
             });
         });
 
-        // it('should throw an error if the request fails', async () => {
-        //     const mockError = new Error('Failed to delete user');
-        //     fetchMock.mockRejectOnce(mockError);
-        //     const userId = 123;
-        //     await expect(deleteUser(userId)).rejects.toThrow('Failed to delete user');
-        // });
+        test('should throw an error if the request fails', async () => {
+            const mockError = new Error('Failed to delete user');
+            fetchMock.mockRejectOnce(mockError);
+            const userId = 123;
+            await expect(deleteUser(userId)).rejects.toThrow('Failed to delete user');
+        });
 
-        // it('should resolve with void if the request succeeds', async () => {
-        //     const mockResponse = { ok: true };
-        //     fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
-        //     const userId = 1;
-        //     await expect(deleteUser(userId)).resolves.toBe(undefined);
-        // });
+        test('should resolve with void if the request succeeds', async () => {
+            const mockResponse = {ok: true};
+            fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+            const userId = 1;
+            await expect(deleteUser(userId)).resolves.toBe(undefined);
+        });
     });
 });
